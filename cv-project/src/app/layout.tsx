0e5dddd1f0bfda9d58c73ca8f0e193e919c452ee
haxuanhuy1 <huyha866@gmail.com>
2024-01-09 01:35:18 +0700

    lan3

--- cv-project/src/app/layout.tsx
+++ cv-project/src/app/layout.tsx
@@ -2,6 +2,7 @@ import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import Home from './page'
+import About from './about/page'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -17,7 +18,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-    <Home />
+    <body>{children}</body>
     </html>
   )
 }
