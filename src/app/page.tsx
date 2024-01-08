daefbb0a782c742e4feb7dcb84dca6c1775b66cc
haxuanhuy1 <huyha866@gmail.com>
2024-01-09 01:36:38 +0700

    lan3

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from 'next/image'
+import styles from './page.module.css'
+import Header from './Header/Header'
+export default function Home() {
+  return (
+   <div>
+    <Header />
+  <p>học viện bưu chính viễn thông</p>
+   </div>
+  )
+}
