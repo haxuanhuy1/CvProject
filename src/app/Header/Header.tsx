8178bb9c89ce53d83819b0c5639e033c8b4490f1
haxuanhuy1 <huyha866@gmail.com>
2024-01-09 02:13:02 +0700

    demo1

--- src/app/Header/Header.tsx
+++ src/app/Header/Header.tsx
@@ -1,7 +1,17 @@
+import clsx from "clsx";
+import Styles from './Header.module.scss'
 const Header = ()=>{
+    const anh1 = '/img/anh1.webp'
+    const LogoGeavn = '/img/Logo.jpg'
     return(
-        <div>
+        <div className={clsx(Styles.header)}>
+            <img src={anh1}></img>
+            <div className={clsx(Styles.header1)}>
+            {/* <img src={LogoGeavn}> </img> */}
+            <img src={LogoGeavn}></img>
+            </div>
         <p>Phần Header</p>
+        <p>Phần Content</p>
         </div>
     )
 }
